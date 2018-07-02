2c23593b58f2e7e3b1bb68301b2aaf84ba6ca64a
hl162981 <hl162981@alibaba-inc.com>
2018-07-02 22:16:45 +0800

    lab4提交

--- week4/lab4.py
+++ week4/lab4.py
@@ -6,7 +6,6 @@
 # the data comes from the BOOYA observations in the Purdue Pulsar Lab
 
 
-import re
 
 def read(file_name_1,file_name_2):
     print ("")
@@ -18,15 +17,37 @@ def read(file_name_1,file_name_2):
     print ("Combining data to a single list " + file_name_1 + ".temp" )
     line_list = []
     line_1 = file_1.readline().strip()  # 调用文件的 readline()方法
-    line_2 = file_2.readline()
+    line_2 = file_2.readline().strip()  # 调用文件的 readline()方法
     line_list.append(line_1.strip() + line_2.strip())
     while line_1:
         line_1 = file_1.readline()
         line_2 = file_2.readline()
-        line_list.append(line_1.strip()+line_2.strip())
+        lint_str = line_1.strip()+line_2.strip()
+        if(len(lint_str)>0):
+            line_list.append(lint_str)
     file_1.close()
     file_2.close()
     return line_list
+
+def report(file_list):
+    count_map = {}
+    row_list = ['M','A','K','E','R','S'];
+    for row in row_list:
+        for column in [1,2,3,4,5]:
+             count_map[row+str(column)]=0
+    for data in file_list:
+        count_map[data] = count_map[data] +1
+    val_list = []
+    for (k, v) in count_map.items():
+        val_list.append(v)
+    print ("           1            2            3            4              5")
+    print ("           =======================================================")
+    for i in range(0,30):
+        if ((i>1)&(i%5==0)):
+            print ("%s           %s          %s           %s           %s            %s"%(row_list[int((i-1)/5)],val_list[i-5],val_list[i-4],val_list[i-3],val_list[i-2],val_list[i-1]))
+
+
+
 
 
 def main():
@@ -42,5 +63,8 @@ def main():
             print(file_list[combine] , end='')
         else:
             print(file_list[combine]+",", end='')
+    print("")
+    report(file_list)
 
 main()
+
